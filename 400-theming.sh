7623d05fd58cf943fbbb62bfc4ac259d40a8deff
Mehdi83 <Mehdy1982_me@yahoo.com>
2022-09-29 23:55:52 -0500

    Themeing_wallpaper_icon manager

---
+++ 400-theming.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+echo "pacman inistallations"
+ 
+sudo pacman -S variety  --noconfirm  --needed  #wallpaper manager
+
+sudo pacman -S arc-gtk-theme --noconfirm  --needed 
+
+
+echo "AUR installations"
+
+packer -S sardi-icons sardi    
+packer -S sardi-mono-papirus-colora-variations-icons-git
+
+packer -S xcursor-breeze
+
+
+echo "################################################################"
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
+echo "################################################################"
+
+
+
+
