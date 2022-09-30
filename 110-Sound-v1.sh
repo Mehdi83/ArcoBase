1ae97005968fd5290919eecd5dc5f5caa4062640
Mehdi83 <Mehdy1982_me@yahoo.com>
2022-09-30 00:33:01 -0500

    Audio installer

---
+++ 110-Sound-v1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+
+
+# -- nonconfirm = ByPass any and all ""Are you sure ?"
+# --needed =  Do not reinstall /if is up-to-date  
+
+sudo pacman -S pulseaudio pulseaudio-alsa --noconfirm --needed
+sudo pacman -S alsa-utils alsa-plugins alsa-firmware alsa-lib --noconfirm --needed
+
+ sudo pacman -S gst-plugins-good gst-plugins-bad gst-plugins-ugly gstreamer --noconfirm --needed
+
+
+
+
+
+
+
+
+
+echo "################################################################"
+echo "###################    Installation Done  ###############Mehdi##"
+echo "################################################################"
