ccd937f7bbf05dbf4cd3ec54b082d151f49a3f41
Mehdi83 <Mehdy1982_me@yahoo.com>
2022-09-30 19:27:52 -0500

    Printer-network printer

---
+++ 130-printers.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+echo "pacman inistallations"
+ 
+sudo pacman -S cups  --noconfirm  --needed   
+sudo pacman -S cups-pdf  --noconfirm  --needed   #save to pdf
+
+sudo pacman -S foomatic-db foomatic-db-engine --noconfirm  --needed # The linux Foundation's OpenPrinting workgroup's
+sudo pacman -S foomatic-db-ppds foomatic-db-nonfree foomatic-db-nonfree-ppds --noconfirm  --needed
+sudo pacman -S foomatic-db-gutenprint-ppds --noconfirm  --needed
+
+echo "###Systemctl installations###"
+
+sudo systemctl enable cups.service   cups.socket
+ 
+
+
+echo "################################################################"
+echo "################################################################"
+echo "###################    T H E   E N D      ###############Mehdi##"
+echo "################################################################"
+echo "################################################################"
+
+
+
+
