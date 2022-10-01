dac638cabad39f50e743a436e9bf38b98fe06bea
Mehdi83 <Mehdy1982_me@yahoo.com>
2022-09-30 21:56:49 -0500

    Teawk dev tools

--- 400-theming.sh
+++ 400-theming.sh
@@ -8,11 +8,15 @@ set -e
 ##################################################################################################################
 
 echo "pacman inistallations"
+
+sudo pacman -S base-devel	 --noconfirm  --needed
+sudo pacman -S git	 --noconfirm  --needed
  
 sudo pacman -S variety  --noconfirm  --needed  #wallpaper manager
 
 sudo pacman -S arc-gtk-theme --noconfirm  --needed 
 
+sudo pacman -S conky #conky-lua-archer conky-manager
 
 echo "AUR installations"
 
--- utilitiee.sh
+++ utilitiee.sh
@@ -4,8 +4,14 @@ set -e
 # -- nonconfirm = ByPass any and all ""Are you sure ?"
 # --needed =  Do not reinstall /if is up-to-date  
 
+sudo pacman -S gnome-tweak-tool --noconfirm --needed
+sudo pacman -S archlinux-tweak-tool-dev-git --noconfirm --needed
+
 sudo pacman -S meld --noconfirm --needed
 sudo pacman -S sublime-text-4 --noconfirm --needed 
+
+sudo pacman -S pamac pamac-gtk --noconfirm --needed 
+
 
 
 echo "################################################################"
