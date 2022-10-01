57a365968a287bc2dd7cec7c3ab9c8857b476db4
Mehdi83 <Mehdy1982_me@yahoo.com>
2022-09-30 19:35:18 -0500

    Printer

--- 130-printers.sh
+++ 130-printers.sh
@@ -12,9 +12,12 @@ echo "pacman inistallations"
 sudo pacman -S cups  --noconfirm  --needed   
 sudo pacman -S cups-pdf  --noconfirm  --needed   #save to pdf
 
-sudo pacman -S foomatic-db foomatic-db-engine --noconfirm  --needed # The linux Foundation's OpenPrinting workgroup's
-sudo pacman -S foomatic-db-ppds foomatic-db-nonfree foomatic-db-nonfree-ppds --noconfirm  --needed
-sudo pacman -S foomatic-db-gutenprint-ppds --noconfirm  --needed
+sudo pacman -S foomatic-db foomatic-db-engine	--noconfirm  --needed # The linux Foundation's OpenPrinting workgroup's
+sudo pacman -S foomatic-db-ppds foomatic-db-nonfree foomatic-db-nonfree-ppds	--noconfirm  --needed
+sudo pacman -S foomatic-db-gutenprint-ppds	--noconfirm  --needed
+
+
+sudo pacman -S system-config-printer	--noconfirm  --needed
 
 echo "###Systemctl installations###"
 
